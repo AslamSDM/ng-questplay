964d8ffa74c1403cecb8e2bf929bf01317d633dd
AslamSDM <aslamprpd@gmail.com>
2024-10-13 13:48:39 +0530

    gas op

--- campaigns/gas-optimization/solidity-optimization/contracts/Challenge.sol
+++ campaigns/gas-optimization/solidity-optimization/contracts/Challenge.sol
@@ -2,14 +2,13 @@
 pragma solidity ^0.8.19;
 
 contract Challenge {
-
-    uint256 _SKIP;
+    uint256 immutable _SKIP;
 
     constructor(uint256 skip) {
         _SKIP = skip;
     }
 
-    /** 
+    /**
      * @notice Returns the sum of the elements of the given array, skipping any SKIP value.
      * @param array The array to sum.
      * @return sum The sum of all the elements of the array excluding SKIP.
@@ -17,9 +16,17 @@ contract Challenge {
     function sumAllExceptSkip(
         uint256[] calldata array
     ) public view returns (uint256 sum) {
-
-        // IMPLEMENT HERE
-        
+        uint256 length = array.length;
+        for (uint256 i = 0; i < length; ) {
+            uint256 value = array[i];
+            if (value != _SKIP) {
+                require(sum + value >= sum, "Overflow");
+                sum = sum + value;
+            }
+            unchecked {
+                i = i + 1;
+            }
+        }
+        return sum;
     }
-
 }
