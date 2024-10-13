11d9247f157b312ad46a10854a7afbb905f203f4
AslamSDM <aslamprpd@gmail.com>
2024-10-13 14:03:03 +0530

    optimised

--- campaigns/gas-optimization/assembly-optimization/contracts/Challenge.sol
+++ campaigns/gas-optimization/assembly-optimization/contracts/Challenge.sol
@@ -2,20 +2,32 @@
 pragma solidity ^0.8.19;
 
 abstract contract Challenge {
-
     /**
      * @notice Returns a copy of the given array in a gas efficient way.
      * @dev This contract will be called internally.
      * @param array The array to copy.
      * @return copy The copied array.
      */
-    function copyArray(bytes memory array) 
-        internal 
-        pure 
-        returns (bytes memory copy) 
-    {
-
-         // IMPLEMENT THIS FUNCTION
-
+    function copyArray(
+        bytes memory array
+    ) internal pure returns (bytes memory copy) {
+        assembly {
+            let length := mload(array)
+            copy := mload(0x40)
+            mstore(copy, length)
+            let src := add(array, 0x20)
+            let dest := add(copy, 0x20)
+            // Copy the data
+            for {
+                let end := add(src, length)
+            } lt(src, end) {
+                src := add(src, 0x20)
+                dest := add(dest, 0x20)
+            } {
+                mstore(dest, mload(src))
+            }
+            // Update the free memory pointer
+            mstore(0x40, add(dest, 0x20))
+        }
     }
 }
